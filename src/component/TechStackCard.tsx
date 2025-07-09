a25c0e5c51db1bf53d83efde80dad42d46d4df11
Théo Ischia <theoischia@gmail.com>
2025-07-09 23:40:17 +0200

    fix(TechStackCard.tsx): removing fixed size

--- src/component/TechStackCard.tsx
+++ src/component/TechStackCard.tsx
@@ -8,7 +8,7 @@ export default function TechStackCard({ items }: { items: TechItem[] }) {
   return (
     <section className="space-y-3">
       <h2 className="ml-3  text-2xl font-semibold text-light">Tech Stack</h2>
-      <div className="card w-[310px]">
+      <div className="card ">
         {/* Flex-wrap grid of tech items */}
         <div className="flex flex-row flex-wrap gap-y-5 gap-x-2 items-start justify-center ">
           {items.map((item, idx) => (
@@ -16,7 +16,7 @@ export default function TechStackCard({ items }: { items: TechItem[] }) {
               key={idx}
               className="flex items-center justify-center gap-2 bg-secondary rounded-4xl p-3 min-w-[120px] max-w-[40%] h-[76px] "
             >
-              <div className=" sm:w-[42px] sm: h-[50px] flex items-center justify-center">
+              <div className=" sm:w-[42px] sm:h-[50px] flex items-center justify-center">
                 {item.icon}
               </div>
               <div className="hidden sm:flex flex-col items-start justify-center max-w-2/5">
