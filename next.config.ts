271d3778be9ea3d9e7c0f6a4f4c27621cf4880dc
Théo Ischia <theoischia@gmail.com>
2025-07-21 23:09:44 +0200

    refactor: going back to use vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-
+/* eslint-disable */
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
+const nextConfig: import("next").NextConfig = {
   webpack(config) {
     // 1. Find the default rule that handles SVGs (Next.js treats SVGs as file assets).
-    const fileLoaderRule = config.module.rules.find((rule: any) =>
-      rule.test && rule.test.toString().includes('svg')
+    const fileLoaderRule = config.module.rules.find(
+      (rule: any) => rule.test && rule.test.toString().includes("svg"),
     );
     if (fileLoaderRule) {
       // 2. Exclude .svg from the default file-loader so we can handle them with SVGR
@@ -15,8 +15,8 @@ const nextConfig: import('next').NextConfig = {
     // 3. Add a new rule: whenever you import a .svg from JS/TS, use @svgr/webpack
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,     // only apply to JS/TS files
-      use: ['@svgr/webpack'],   // run SVGR loader
+      issuer: /\.[jt]sx?$/, // only apply to JS/TS files
+      use: ["@svgr/webpack"], // run SVGR loader
     });
 
     return config;
@@ -25,3 +25,5 @@ const nextConfig: import('next').NextConfig = {
 
 module.exports = nextConfig;
 export default nextConfig;
+
+/* eslint-enable */
